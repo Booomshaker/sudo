bd57467f12712b84384515b153061d6b3911a3fb
Booomshaker <604561221@qq.com>
2017-09-15 22:36:37 +0800

    modify judge way

--- README.md
+++
@@ -1,5 +0,0 @@
-##概述
-采用了回溯法生成数独终盘
-
-##参考
-回溯法生成数独(http://blog.csdn.net/xiahn1a/article/details/50852849)
--- sudoku/sudoku.cpp
+++ sudoku/sudoku.cpp
@@ -21,7 +21,7 @@ public:
 		if (para != "-c")
 			return false;
 		for (int k = 0; k < num_str.length(); k++) {
-			if (num_str[k] >= '9' || num_str[k] <= '0')
+			if (num_str[k] > '9' || num_str[k] < '0')
 				return false;
 		}
 		return true;
@@ -78,13 +78,16 @@ public:
 	}
 	void toPrint () {
 		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++)
-				cout << arr[i][j] << " ";
-			cout << endl;
+			for (int j = 0; j < 9; j++) {
+				putchar(arr[i][j] + '0');
+				putchar(' ');
+			}
+			putchar('\n');
 		}
 	}
 	void outPut(bool isRight) {         //生成并输出数独终盘
 		if (isRight) {						//若输入格式正确 则输出数独终盘
+			freopen("sudoku.txt", "w", stdout);
 			int cnt = atoi(num_str.c_str());
 			for (int k = 0; k < cnt; k++) {
 				shuf(temp);
@@ -93,11 +96,12 @@ public:
 				toPrint(); 
 				memset(temp, 0, sizeof(temp));
 				memset(arr, 0, sizeof(arr));
-				cout << endl;
+				putchar('\n');
 			}
 		}
 		else {								//若输入格式错误 则报错
-			cout << "Format Wrong!!!" << endl;
+			freopen("CON", "w", stdout);
+			cout << "Format Wrong!!" << endl;
 		}
 	}
 };
@@ -106,7 +110,7 @@ public:
 
 int main(int argc, char ** argv) {
 	srand((unsigned)time(0));
-	freopen("sudoku.txt", "w", stdout);
+	
 	Judge ju;
 	todoSudoku to;
 	para = argv[argc - 2];
